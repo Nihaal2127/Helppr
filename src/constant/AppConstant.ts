e9798033a29c154cc0244b96f61ee15b67f92134
ravina14 <sanghaniravina1401@gmail.com>
2025-10-14 10:19:28 +0530

    device token adde in login time
    device token adde in login time

--- src/constant/AppConstant.ts
+++ src/constant/AppConstant.ts
@@ -1,9 +1,11 @@
 export const AppConstant = {
     BASE_URL: "https://csx88sd9ed.execute-api.ap-south-1.amazonaws.com/prod/api",//Help Pr Live
     IMAGE_BASE_URL: "https://d2snwgkdggvp65.cloudfront.net/",//Help Pr Live
+    // BASE_URL: "http://localhost:5001/api",
     // BASE_URL: "https://raamisegei.execute-api.us-east-1.amazonaws.com/dev/api",
     // IMAGE_BASE_URL: "https://d2d4noj5f8gqer.cloudfront.net/",
     authToken: "authToken",
+    deviceToken: "deviceToken",
     isAdmin: "isAdmin",
     adminId: "adminId",
     partnerId: "partnerId",
