5a47ee0dd940f849f04d20011d0f3208052bdb2e
bhaskarmalasani <bhaskararao1.m@gmail.com>
2026-04-03 18:22:01 +0530

    Update AppConstant.ts

--- src/constant/AppConstant.ts
+++ src/constant/AppConstant.ts
@@ -1,5 +1,5 @@
 export const AppConstant = {
-    BASE_URL: "https://csx88sd9ed.execute-api.ap-south-1.amazonaws.com/prod/api",//Help Pr Live
+    BASE_URL: "https://dtvx6yflf1.execute-api.ap-south-1.amazonaws.com/staging/api",//Help Pr Live
     IMAGE_BASE_URL: "https://d2snwgkdggvp65.cloudfront.net/",//Help Pr Live
     // BASE_URL: "http://localhost:5001/api",
     // BASE_URL: "https://raamisegei.execute-api.us-east-1.amazonaws.com/dev/api",
